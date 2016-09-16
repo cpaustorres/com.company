88d54f58bf1b44ecd7a2199a884d139a81861943
cpaustorres <paus54@gmail.com>
2016-09-16 17:07:00 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+
+        Coche coche1 = new Coche("Jaguar","XF","3453WRD",4000,80000.00);
+        Coche coche2 = new Coche("Mercedes","Benz","3234RRRD",8000,160000.00);
+        Coche coche3 = new Coche("BMW","M3","4853WRD",4000,80000.00);
+
+        List<Coche> cocheList = new ArrayList<>();
+
+        cocheList.add(coche1);
+        cocheList.add(coche2);
+        cocheList.add(coche3);
+
+        System.out.println("Muestro el coche1:" + cocheList.get(0));
+    }
+}
